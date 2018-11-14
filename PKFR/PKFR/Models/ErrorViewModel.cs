d91672b3c158b61888474f4623e5d75cb9288bdb
Zarodath <32298596+Zarodath@users.noreply.github.com>
2018-11-14 12:04:39 +0200

    Initial project commit

---
+++ PKFR/PKFR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PKFR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
